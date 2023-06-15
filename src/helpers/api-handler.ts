3fb2c19980a260f156ce8712b10db68a46b4a93f
Bob Baxter <bob.e.baxter@gmail.com>
2023-06-14 21:18:12 -0500

    added endpoints for cargo, chart, dock, and refuel

---
+++ src/helpers/api-handler.ts
@@ -0,0 +1,41 @@
+import { NextApiResponse } from 'next';
+
+export async function apiHandler({
+  allowedMethods,
+  body = undefined,
+  method = 'GET',
+  res,
+  url,
+}: {
+  allowedMethods: string[];
+  body?: object | undefined;
+  method: string | undefined;
+  res: NextApiResponse;
+  url: string;
+}) {
+  if (allowedMethods.includes(method)) {
+    const response = await fetch(url, {
+      method,
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${process.env.GAME_API_TOKEN}`,
+      },
+      body: body ? JSON.stringify(body) : undefined,
+    });
+
+    if (!response.ok) {
+      const body = await response.json();
+      if (body && body.error) {
+        return res.status(response.status).json(body);
+      } else {
+        return res.status(response.status).end(response.statusText);
+      }
+    }
+
+    const { data } = await response.json();
+    return res.status(200).json(data);
+  } else {
+    res.setHeader('Allow', allowedMethods);
+    return res.status(405).end(`Method ${method} Not Allowed`);
+  }
+}
---
+++ src/pages/api/my/ships/index.ts
@@ -0,0 +1,16 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { apiHandler } from '@/helpers/api-handler';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { method } = req;
+
+  await apiHandler({
+    allowedMethods: ['GET'],
+    method,
+    res,
+    url: `${process.env.GAME_API_BASE_URL}/my/ships`,
+  });
+}
